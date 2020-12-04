4842269131d12d2df630db35e20906cb872c0d23
Aditya Agrawal <aditya.agrawal0309@gmail.com>
2020-12-04 20:58:09 +0530

    feat: encoded url and added alert messages

--- app.js
+++ app.js
@@ -2,35 +2,36 @@ var btnTranslate = document.getElementById('btn-translate');
 var inputText = document.getElementById('input-text');
 var translatedText = document.getElementById('translated-text');
 
-var url = "https://api.funtranslations.com/translate/minion.json";
+var minionURL = "https://api.funtranslations.com/translate/minion.json";
 
 
 // construct URL
 function addTextParameterToURL(textParameter) {
-  return `${url}?text=${textParameter}`;
+  var endcodedURI = encodeURIComponent(textParameter);
+  return `${minionURL}?text=${endcodedURI}`;
 }
 
 // click handler function
 function clickHandler() {
   var textParameter = inputText.value;
-  
-  // addTextParameterToURL(textParameter);
-  // console.log(url)
+  var serverURL = addTextParameterToURL(textParameter);
 
- 
+  console.log(serverURL);
 
   if (textParameter.length === 0) {
-    alert("you need to write a sentence")
+    alert("You need to write a sentence");
+  } else if (/^\d+$/.test(textParameter)) {
+    alert("You need to include letters in your sentence");
+  } else {
+    fetch(serverURL)
+    .then(response => response.json())
+    .then(json => { 
+      translatedText.innerText = json.contents.translated;
+    })
+    .catch(error => {
+      console.log('error occured', error);
+    })
   }
-
-  fetch(addTextParameterToURL(textParameter))
-  .then(response => response.json())
-  .then(json => {
-    translatedText.innerText = json.contents.translated;
-  })
-  .catch(error => {
-    console.log('error occured', error);
-  })
 }
 
 btnTranslate.addEventListener('click', clickHandler);
